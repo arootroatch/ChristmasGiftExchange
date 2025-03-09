adaf3bd271da09684144bae595ca0a5326a9e34a
Alex Root-Roatch <arootroatch@gmail.com>
2025-03-09 00:59:00 -0600

    submitEmail tests

--- resources/js/serverless.js
+++ resources/js/serverless.js
@@ -1,140 +1,151 @@
-import { showSnackbar } from "./scripts.js";
+import {showSnackbar} from "./scripts.js";
 import state from "./state.js";
 
-
-document.getElementById("emailQueryBtn").addEventListener("click", getName);
-
-export function submitEmails(event) {
-  event.preventDefault();
-
-  const btn = document.getElementById("submitEmails");
-  btn.innerHTML = "Loading...";
-  btn.style.color = "#808080";
-  const emailInputs = Array.from(document.getElementsByClassName("emailInput"));
-  // create an array of objects with names, emails, and which index in the givers array
-  const emails = emailInputs.map((input) => {
-    return {
-      name: input.name,
-      email: input.value.trim(),
-      index: input.id,
-    };
-  });
-
-  // update each giver object with the matching email
-  let random = Math.random().toString(20);
-  let date = new Date().toISOString();
-  emails.forEach((obj) => {
-    let i = parseInt(obj.index);
-    state.givers[i].email = obj.email;
-    state.givers[i].id = `${state.givers.length}_${random}_${date}`;
-    state.givers[i].date = date;
-  });
-
-  postToServer();
-
-  return false;
+function setLoadingState(thing) {
+    const btn = document.getElementById(thing);
+    btn.innerHTML = "Loading...";
+    btn.style.color = "#808080";
 }
 
-document
-  .getElementById("emailTableBody")
-  .addEventListener("submit", (event) => submitEmails(event));
+export function getEmails() {
+    const emailInputs = Array.from(document.getElementsByClassName("emailInput"));
+    return emailInputs.map((input) => {
+        return {
+            name: input.name,
+            email: input.value.trim(),
+            index: input.id
+        };
+    });
+}
 
-export async function postToServer() {
-  const options = {
-    method: "POST", // *GET, POST, PUT, DELETE, etc.
-    mode: "cors",
-    body: JSON.stringify(state.givers), // GET requests can't have a body
-  };
-
-  let results = await fetch("/.netlify/functions/postToDb", options).then(
-    (response) => {
-      if (response.status === 200) {
-        const sendDiv = document.getElementById("sendEmails");
-        sendDiv.innerHTML = `
+function displaySendEmails() {
+    const sendDiv = document.getElementById("sendEmails");
+    sendDiv.innerHTML = `
           <p>${state.givers.length} email addresses added successfully!</p>
           <p>Now let's send out those emails:</p>
           <button class="button" id="sendEmailsBtn">Send Emails</button>
         `;
-        sendDiv.classList.replace("hidden", "show");
-        document
-          .getElementById("sendEmailsBtn")
-          .addEventListener("click", batchEmails);
+    sendDiv.classList.replace("hidden", "show");
+    document
+        .getElementById("sendEmailsBtn")
+        .addEventListener("click", batchEmails);
+}
 
-        // hide the emails table
-        const table = document.getElementById("emailTable");
-        table.classList.add("hide");
-        setTimeout(() => {
-          table.classList.replace("show", "hidden");
-          table.classList.remove("hide");
-        }, 500);
-      } else {
-        console.log(response.body);
-      }
-    }
-  );
+function hideEmailTable() {
+    const table = document.getElementById("emailTable");
+    table.classList.add("hide");
+    setTimeout(() => {
+        table.classList.replace("show", "hidden");
+        table.classList.remove("hide");
+    }, 500);
+}
+
+function updateStateWithEmails(emails) {
+    let random = Math.random().toString(20);
+    let date = new Date().toISOString();
+    emails.forEach((obj) => {
+        let i = parseInt(obj.index);
+        state.givers[i].email = obj.email;
+        state.givers[i].id = `${state.givers.length}_${random}_${date}`;
+        state.givers[i].date = date;
+    });
+}
+
+export function submitEmails(event) {
+    event.preventDefault();
+    setLoadingState("submitEmails");
+    const emails = getEmails();
+    updateStateWithEmails(emails);
+
+    postToServer().then(
+        (response) => {
+            if (response.status === 200) {
+                displaySendEmails();
+                hideEmailTable();
+            } else {
+                console.log(response.body);
+            }
+        }
+    );
+
+    return false;
+}
+
+document
+    .getElementById("emailTableBody")
+    .addEventListener("submit", (event) => submitEmails(event));
+
+export async function postToServer() {
+    const options = {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors",
+        body: JSON.stringify(state.givers), // GET requests can't have a body
+    };
+    return await fetch("/.netlify/functions/postToDb", options)
 }
 
 function batchEmails() {
-  const btn = document.getElementById("sendEmails");
-  btn.innerHTML = "Loading...";
-  btn.style.color = "#808080";
+    setLoadingState("sendEmails");
 
-  let i = 0;
-  let count = 0;
-  let promises = state.givers.map(async (giver) => {
-    i++;
-    await fetch("/.netlify/functions/dispatchEmail", {
-      method: "POST",
-      mode: "cors",
-      body: JSON.stringify({
-        name: giver.name,
-        recipient: giver.recipient,
-        email: giver.email,
-      }),
-    }).then((response) => {
-      if (response.status === 200) {
-        count++;
-      }
+    let i = 0;
+    let count = 0;
+    let promises = state.givers.map(async (giver) => {
+        i++;
+        await fetch("/.netlify/functions/dispatchEmail", {
+            method: "POST",
+            mode: "cors",
+            body: JSON.stringify({
+                name: giver.name,
+                recipient: giver.recipient,
+                email: giver.email,
+            }),
+        }).then((response) => {
+            if (response.status === 200) {
+                count++;
+            }
+        });
     });
-  });
-  Promise.all(promises).then(() => {
-    const sendEmails = document.getElementById("sendEmails");
-    sendEmails.classList.add("hide");
-    setTimeout(() => {
-      sendEmails.classList.replace("show", "hidden");
-      sendEmails.classList.remove("hide");
-    }, 500);
-    showSnackbar(
-      `Sent ${count} of ${state.givers.length} emails successfully!`,
-      "success"
-    );
-  });
+    Promise.all(promises).then(() => {
+        const sendEmails = document.getElementById("sendEmails");
+        sendEmails.classList.add("hide");
+        setTimeout(() => {
+            sendEmails.classList.replace("show", "hidden");
+            sendEmails.classList.remove("hide");
+        }, 500);
+        showSnackbar(
+            `Sent ${count} of ${state.givers.length} emails successfully!`,
+            "success"
+        );
+    });
 }
 
-async function getName(e) {
-  e.preventDefault;
-  let email = document.getElementById("emailQuery").value;
-  const btn = document.getElementById("emailQueryBtn");
-  btn.innerHTML = "Loading...";
-  btn.style.color = "#808080";
 
-  const options = {
-    method: "POST", // *GET, POST, PUT, DELETE, etc.
-    mode: "cors",
-    body: email, // GET requests can't have a body
-  };
-  let errorMsg = "";
-  let results = await fetch("/.netlify/functions/get_name", options)
-    .then((response) => response.json())
-    .catch((error) => (errorMsg = error));
-  if (errorMsg !== "") {
-    document.getElementById("query").innerHTML = `
+document.getElementById("emailQueryBtn").addEventListener("click", getName);
+
+async function getName(e) {
+    e.preventDefault;
+    let email = document.getElementById("emailQuery").value;
+    const btn = document.getElementById("emailQueryBtn");
+    btn.innerHTML = "Loading...";
+    btn.style.color = "#808080";
+
+    const options = {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors",
+        body: email, // GET requests can't have a body
+    };
+    let errorMsg = "";
+    let results = await fetch("/.netlify/functions/get_name", options)
+        .then((response) => response.json())
+        .catch((error) => (errorMsg = error));
+    if (errorMsg !== "") {
+        document.getElementById("query").innerHTML = `
     <div style="color:#b31e20">
         Email address not found!
     </div>
     `;
-    setTimeout(() => {
-      document.getElementById("query").innerHTML = `
+        setTimeout(() => {
+            document.getElementById("query").innerHTML = `
         <label for="emailQuery">
             Need to know who you're buying a gift for?
         </label>
@@ -155,15 +166,15 @@ async function getName(e) {
             </button>
         </div>
       `;
-    }, 2000);
-  } else {
-    let timestamp = Date.parse(results.date);
-    let date = new Date(timestamp);
-    document.getElementById("query").innerHTML = `
+        }, 2000);
+    } else {
+        let timestamp = Date.parse(results.date);
+        let date = new Date(timestamp);
+        document.getElementById("query").innerHTML = `
       <div>
           As of ${date.toDateString()}, you're buying a gift for  <span>${
-      results.recipient
-    }!</span>
+            results.recipient
+        }!</span>
     </div>
     <div>
             <input
@@ -182,5 +193,5 @@ async function getName(e) {
             </button>
         </div>
     `;
-  }
+    }
 }
--- tests/serverless.spec.js
+++ tests/serverless.spec.js
@@ -1,28 +1,119 @@
 // @vitest-environment jsdom
 
-import {expect, test, vi} from 'vitest'
-import {submitEmails, postToServer} from '/resources/js/serverless.js'
-import {JSDOM} from "jsdom";
+import {beforeEach, describe, expect, it, vi} from 'vitest'
+import {getEmails, submitEmails} from '/resources/js/serverless.js'
+import state from "../resources/js/state";
 
-global.fetch = vi.fn(() => Promise.resolve({json: () => Promise.resolve({})}));
+function renderEmailTable(givers) {
+    const body = document.getElementById("emailTableBody");
+    for (let i = 0; i < givers.length; i++) {
+        body.insertAdjacentHTML(
+            "afterbegin",
+            `<div class="emailDiv">
+          <label for=${i}>${givers[i].name}</label>
+          <input type="email" class="emailInput" maxlength="100" name=${givers[i].name} id=${i} value=${givers[i].email} required/>
+        </div>
+        `
+        );
+    }
+}
 
-const clickEvent = new CustomEvent('myCustomEvent', {
-    detail: {
-        message: 'Hello from custom event',
-        targetElement: document.getElementById('emailTableBody') // Set your desired target element here
-    },
-    bubbles: true, // Allows the event to bubble up the DOM tree
-    cancelable: true // Allows the event to be canceled
-});
+function installGivers(giverNames) {
+    giverNames.forEach((name) => {
+        state.givers.push({name: name, recipient: "", email: "", date: "", id: ""});
+    })
+}
 
-test('submitEmails', () => {
+
+describe('serverless', () => {
+    global.fetch = vi.fn(() => Promise.resolve({status: 200}));
+    Math.random = vi.fn(() => 123456789);
+    vi.useFakeTimers();
+    vi.setSystemTime(new Date(2023, 0, 1));
     vi.mock(import("/resources/js/serverless.js"), async (importOriginal) => {
         const actual = await importOriginal()
         return {
             ...actual,
-            postToServer: vi.fn(() => 'Mocked result'),
+            getEmails: vi.fn(actual.getEmails),
+            postToServer: vi.fn(actual.postToServer),
         }
     })
+    renderEmailTable([
+        {name: "Alex", email: "arootroatch@gmail.com"},
+        {name: "Whitney", email: "whitney@gmail.com"},
+        {name: "Hunter", email: "hunter@gmail.com"},
+        {name: "Megan", email: "megan@gmail.com"}]);
+    installGivers(["Alex", "Whitney", "Hunter", "Megan"]);
 
-    const result = submitEmails(clickEvent);
+    describe("submitEmails", () => {
+        const submitEmailsButton = document.getElementById("submitEmails");
+        beforeEach(() => {
+            const emailTableBody = document.getElementById("emailTableBody");
+            const submitEvent = new Event("submit", {bubbles: true, cancelable: true});
+            emailTableBody.dispatchEvent(submitEvent);
+        })
+
+        it('sets button text to Loading...', () => {
+            expect(submitEmailsButton.innerHTML).toBe('Loading...');
+        })
+
+        it("sets button color to #808080", () => {
+            expect(submitEmailsButton.style.color).toBe("rgb(128, 128, 128)");
+        })
+
+        it("gets emails from form", () => {
+            const result = getEmails();
+            expect(result).toContainEqual({"name": "Alex", "email": "arootroatch@gmail.com", "index": "0"});
+            expect(result).toContainEqual({"name": "Whitney", "email": "whitney@gmail.com", "index": "1"});
+            expect(result).toContainEqual({"name": "Hunter", "email": "hunter@gmail.com", "index": "2"});
+            expect(result).toContainEqual({"name": "Megan", "email": "megan@gmail.com", "index": "3"});
+        })
+
+        it("invokes getEmails", () => {
+            expect(getEmails).toHaveBeenCalled();
+        })
+
+        it("sets email, id, and date for each giver", () => {
+            const date = new Date().toISOString();
+            const givers = [
+                [state.givers[0], "arootroatch@gmail.com"],
+                [state.givers[1], "whitney@gmail.com"],
+                [state.givers[2], "hunter@gmail.com"],
+                [state.givers[3], "megan@gmail.com"]];
+
+            givers.forEach(([giver, email]) => {
+                expect(giver.email).toBe(email);
+                expect(giver.date).toBe(date);
+                expect(giver.id).toBe(`4_1ibc1j9_${date}`);
+            })
+        })
+
+        it("invokes postToServer", async () => {
+            expect(global.fetch).toHaveBeenCalledWith("/.netlify/functions/postToDb",
+                {"body": JSON.stringify(state.givers), "method": "POST", "mode": "cors"});
+        })
+
+        it("displays saved emails success message", async () => {
+            const sendDiv = document.getElementById("sendEmails");
+            expect(sendDiv.innerHTML).toContain("4 email addresses added successfully!");
+            expect(sendDiv.innerHTML).toContain("Now let's send out those emails:");
+            expect(sendDiv.innerHTML).toContain(`<button class="button" id="sendEmailsBtn">Send Emails</button>`);
+            expect(sendDiv.classList).not.toContain("hidden");
+            expect(sendDiv.classList).toContain("show");
+        })
+
+        it("hides email table after saving emails", async () => {
+            const table = document.getElementById("emailTable");
+            expect(table.classList).toContain("hide");
+            setTimeout(() => {
+                expect(table.classList).not.toContain("hide");
+                expect(table.classList).not.toContain("show");
+                expect(table.classList).toContain("hidden");
+            })
+        })
+
+    })
+
 })
+
+
